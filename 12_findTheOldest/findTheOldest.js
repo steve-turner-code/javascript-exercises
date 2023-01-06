a44f110375bd71c0610df87a11ff1c989b9086db
Stephen Turner <steve.turner.1993@gmail.com>
2023-01-06 20:08:40 +1100

    Finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((oldest, person) => {
+        const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        const currentAge = person.yearOfDeath - person.yearOfBirth;
 
+        return oldestAge < currentAge ? person : oldest;
+    }
+        
+        
+        
+        )
 };
+
+function getAge (person) {
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
