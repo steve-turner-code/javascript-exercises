76c7efa469696c9c3e1ded30c88e7adb47976597
Stephen Turner <steve.turner.1993@gmail.com>
2023-01-06 15:43:59 +1100

    Finished the palindromes and fibonacci exercies

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+const lowerCase = string.toLowerCase();
+const reversedString = lowerCase
+    .split('')
+    .reverse()
+    .join('')
+
+return (lowerCase === reversedString);
+
 
 };
 
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let previous = 0;
+    let current = 1;
 
+
+    for (let i = 1; i<index; i++) {
+        let currentStore = current
+        current += previous;
+        previous = currentStore;
+    }
+return current;
 };
 
 // Do not edit below this line
